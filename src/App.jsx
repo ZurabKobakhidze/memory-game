8ec6f35d05ef4441e676fe1079bba9d5905a0e09
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-06-16 01:15:23 +0400

    first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
+import logo from "./assets/logo.svg";
 
-import './App.css'
 
 function App() {
-  
-
   return (
     <>
-      
+      <div id="body_div" className="bg-bodyColor min-h-screen flex flex-col items-center pt-20 pl-6 pr-6 box-border">
+      <h1 className="font-bold text-2xl text-white">memory</h1>
+      <div id="container" className="bg-white rounded-[10px] w-full p-[24px] mt-[40px]"></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bodyColor: "#152938",
+        logoColor: "white",
+      },
+    },
+  },
+  plugins: [],
+};
