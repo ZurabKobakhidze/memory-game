58d9b7a0a71ecdf79d72ad94fc74c53a1b3804c3
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-06-19 02:11:20 +0400

    add grid

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function GamePage() {
   const location = useLocation();
-  console.log(location);
   const { theme, players, gridSize } = location.state;
 
+  const generateArray = (size) => {
+    const num = size.split("x")[0];
+    const squareNum = num * num;
+    const halfSquareNum = squareNum / 2;
+    const initialArray = Array.from({length: halfSquareNum}, (_, i) => i + 1);
+    return [...initialArray, ...initialArray];
+  };
 
+  const numbersArray = generateArray(gridSize);
+  const shuffledArray = numbersArray.sort(() => 0.5 - Math.random()); 
 
+  useEffect(() => {
+    console.log("Shuffled array: ", shuffledArray);
+  }, []); 
 
   return (
     <div>
@@ -15,7 +26,11 @@ function GamePage() {
       <p>Theme: {theme}</p>
       <p>Number of Players: {players}</p>
       <p>Grid Size: {gridSize}</p>
-  
+      <div id="grid_container" className="grid grid-cols-4 items-center justify-items-center">
+        {shuffledArray.map((number, index) => (
+          <p key={index}>{number}</p>
+        ))}
+      </div>
     </div>
   );
 }
