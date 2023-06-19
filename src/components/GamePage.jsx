695e25137611e9e0b927a66a47514f862528afd5
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-06-20 00:31:34 +0400

    add icons

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,24 +1,103 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUser,
+  faLock,
+  faCog,
+  faTrash,
+  faSearch,
+  faStar,
+  faHeart,
+  faCamera,
+  faWifi,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faClock,
+  faCalendar,
+  faEnvelope,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faApple,
+  faFacebook,
+  faGoogle,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faHome,
+  faUser,
+  faLock,
+  faCog,
+  faTrash,
+  faSearch,
+  faStar,
+  faHeart,
+  faCamera,
+  faWifi,
+  faClock,
+  faCalendar,
+  faEnvelope,
+  faPaperPlane,
+  faApple,
+  faFacebook,
+  faGoogle,
+  faGithub
+);
+
+const iconsArray = [
+  faHome,
+  faUser,
+  faLock,
+  faCog,
+  faTrash,
+  faSearch,
+  faStar,
+  faHeart,
+  faCamera,
+  faWifi,
+  faClock,
+  faCalendar,
+  faEnvelope,
+  faPaperPlane,
+  faApple,
+  faFacebook,
+  faGoogle,
+  faGithub,
+];
 
 function GamePage() {
   const location = useLocation();
   const { theme, players, gridSize } = location.state;
 
-  const generateArray = (size) => {
+  const generateArray = (size, theme) => {
     const num = size.split("x")[0];
     const squareNum = num * num;
     const halfSquareNum = squareNum / 2;
-    const initialArray = Array.from({length: halfSquareNum}, (_, i) => i + 1);
+
+    let initialArray;
+
+    if (theme === "Numbers") {
+      initialArray = Array.from({ length: halfSquareNum }, (_, i) => ({
+        id: i + 1,
+        value: i + 1,
+      }));
+    } else if (theme === "Icons") {
+      initialArray = Array.from({ length: halfSquareNum }, (_, i) => ({
+        id: i + 1,
+        value: iconsArray[i % iconsArray.length],
+      }));
+    }
+
     return [...initialArray, ...initialArray];
   };
 
-  const numbersArray = generateArray(gridSize);
-  const shuffledArray = numbersArray.sort(() => 0.5 - Math.random()); 
-
-  useEffect(() => {
-    console.log("Shuffled array: ", shuffledArray);
-  }, []); 
+  const gameArray = generateArray(gridSize, theme);
+  const shuffledArray = gameArray.sort(() => 0.5 - Math.random());
 
   return (
     <div>
@@ -26,10 +105,17 @@ function GamePage() {
       <p>Theme: {theme}</p>
       <p>Number of Players: {players}</p>
       <p>Grid Size: {gridSize}</p>
-      <div id="grid_container" className="grid grid-cols-4 items-center justify-items-center">
-        {shuffledArray.map((number, index) => (
-          <p key={index}>{number}</p>
-        ))}
+      <div
+        id="grid_container"
+        className="grid grid-cols-4 items-center justify-items-center"
+      >
+        {shuffledArray.map((item, index) =>
+          theme === "Numbers" ? (
+            <p key={index}>{item.value}</p>
+          ) : (
+            <FontAwesomeIcon key={index} icon={item.value} />
+          )
+        )}
       </div>
     </div>
   );
