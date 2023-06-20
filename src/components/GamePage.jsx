a44bec8e2a6c7634166f838560bd1d4a9347bf87
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-06-20 04:08:37 +0400

    add new functions

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -4,30 +4,9 @@ import iconLogo from "../assets/logo.svg";
 import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHome,
-  faUser,
-  faLock,
-  faCog,
-  faTrash,
-  faSearch,
-  faStar,
-  faHeart,
-  faCamera,
-  faWifi,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  faClock,
-  faCalendar,
-  faEnvelope,
-  faPaperPlane,
-} from "@fortawesome/free-regular-svg-icons";
-import {
-  faApple,
-  faFacebook,
-  faGoogle,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
+import { faHome, faUser, faLock, faCog, faTrash, faSearch, faStar, faHeart, faCamera, faWifi } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faCalendar, faEnvelope, faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import { faApple, faFacebook, faGoogle, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 library.add(
   faHome,
@@ -74,7 +53,8 @@ const iconsArray = [
 function GamePage() {
   const location = useLocation();
   const { theme, gridSize } = location.state;
-  const [players] = useState(location.state.players); // Set the number of players based on the initial selection
+  const [players] = useState(location.state.players);
+  
 
   const gridColumns = gridSize.split("x")[0];
 
@@ -89,11 +69,13 @@ function GamePage() {
       initialArray = Array.from({ length: halfSquareNum }, (_, i) => ({
         id: i + 1,
         value: i + 1,
+        isClicked: false
       }));
     } else if (theme === "Icons") {
       initialArray = Array.from({ length: halfSquareNum }, (_, i) => ({
         id: i + 1,
         value: iconsArray[i % iconsArray.length],
+        isClicked: false
       }));
     }
 
@@ -102,6 +84,12 @@ function GamePage() {
 
   const gameArray = generateArray(gridSize, theme);
   const shuffledArray = gameArray.sort(() => 0.5 - Math.random());
+
+  const [items, setItems] = useState(shuffledArray);
+
+  const handleClick = (index) => {
+    setItems(items.map((item, i) => i === index ? { ...item, isClicked: true } : item));
+  };
 
   return (
     <div>
@@ -112,21 +100,28 @@ function GamePage() {
         style={{ gridTemplateColumns: `repeat(${gridColumns}, 1fr)` }}
         className="grid items-center justify-items-center"
       >
-        {shuffledArray.map((item, index) =>
-          theme === "Numbers" ? (
-            <p key={index}>{item.value}</p>
-          ) : (
-            <FontAwesomeIcon key={index} icon={item.value} />
-          )
-        )}
+        {items.map((item, index) => (
+          <div
+            key={index}
+            id="circle"
+            className={`rounded-full w-[47px] h-[47px] flex justify-center items-center ${item.isClicked ? 'bg-playergray ' : 'bg-bodyColor'}`}
+            onClick={() => handleClick(index)}
+          >
+            {item.isClicked && (theme === "Numbers" ? (
+              <p>{item.value}</p>
+            ) : (
+              <FontAwesomeIcon icon={item.value} />
+            ))}
+          </div>
+        ))}
       </div>
-      <p>Number of Players: {players}</p>
+
       <div className="flex gap-6 justify-center">
         {Array.from({ length: players }, (_, i) => i + 1).map((player) => (
           <div
             key={player}
             id={`player ${player}`}
-            className="w-16 h-18 bg-playergray rounded-sm flex justify-center items-center flex-col"
+            className="w-full h-18 bg-playergray rounded-sm flex justify-center items-center flex-col"
           >
             <p>p{player}</p>
             <p>0</p>
